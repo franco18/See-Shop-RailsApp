a968bcb21c1630534965edfca1ba6f76ad3beb12
Jose Miguel Blanco <jmblanco99@gmail.com>
2014-10-26 17:40:06 -0500

    Attempt deploy on heroku #3

--- spec/controllers/brands_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-RSpec.describe BrandsController, :type => :controller do
-  it "is invalid without a name" do
-    get :index
-    expect(response).to be_success
-  end
-end

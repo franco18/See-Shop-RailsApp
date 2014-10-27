d83b80e6aa05575d751f3ff8b79867026ae7aaa3
Jose Miguel Blanco <jmblanco99@gmail.com>
2014-10-27 08:01:10 -0500

    Fixing toggle bug

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SeeAndShop::Application.config.secret_token = '0d81e6fadc3721b8e5a7ba194d7196a22e78d9d3632b79e51b583b8a6ab8e94cc6e9679ec7e62fab7b6092580a14896a72a345fc227fcf487360228ec769465a'
+SeeAndShop::Application.config.secret_token = '0d81e6fad3721b8e5a7ba194d7196a22e78d9d3632b79e51b583b8a6ab8e94cc6e9679ec7e62fab7b6092580a14896a72a345fc227fcf487360228ec769465a'
